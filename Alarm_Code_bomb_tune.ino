3e7fe19d840f8a9f011bac842a08bbcb16c32e47
Ashish Gupta <ashishgupta1350@gmail.com>
2018-05-24 19:53:57 +0530

    Adding Files.

---
+++ Alarm_Code_bomb_tune.ino
@@ -0,0 +1,33 @@
+int incomingByte = 0;   // for incoming serial data
+int flag=1;
+void setup() {
+        Serial.begin(9600);
+        pinMode(13,OUTPUT);// opens serial port, sets data rate to 9600 bps
+        digitalWrite(13,LOW);
+        flag=1;
+}
+
+void loop() {
+        
+        // send data only when you receive data:
+        if (Serial.available() > 0 and flag) {
+                // read the incoming byte:
+                for (int i=1;i<100;i++){
+                digitalWrite(13,HIGH);
+                delay(60);
+                digitalWrite(13,LOW);
+                delay(60);
+                }
+                Serial.print("I found a burgler ");
+                
+                delay(1000);// It will play for 10 seconds
+                digitalWrite(13, LOW);
+                // say what you got:
+                flag=0;
+                //Serial.println("");
+        }
+        else{
+          digitalWrite(13,LOW);
+          
+        }
+}
